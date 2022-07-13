3d099db195232445933ac95d6332126d6dd0f067
Jakub <87284797+ezterr@users.noreply.github.com>
2022-07-13 13:59:49 +0200

    Delete config.ts

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  jwtSecret:
-    'YrkWsbPh4UMVEcmARkUkadJIj/dmgi2YsEEOIUotbVhUwvkaC1nvezF/4O9nqg79ML7RtKCeOJwrXEUhosXdIA==',
-  jwtTimeToExpire: '1y',
-  jwtCookieTimeToExpire: 1000 * 60 * 60 * 24 * 365, //1y
-  jwtCookieDomain: 'localhost',
-};
